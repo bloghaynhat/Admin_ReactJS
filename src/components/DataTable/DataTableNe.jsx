f72b66a310bf6be6e6a41782595be4b34379cab4
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2025-04-03 07:53:18 +0700

    Save

--- src/components/DataTable/DataTableNe.jsx
+++ src/components/DataTable/DataTableNe.jsx
@@ -1,43 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-import DataTable from 'react-data-table-component';
- 
 
 const DataTableNe = () => {
-    const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // Hàm fetch dữ liệu từ API
+    const fetchData = async () => {
+      try {
+        // Gọi API
+        const response = await fetch(
+          "https://67e369142ae442db76d0029b.mockapi.io/dttb"
+        );
+        // Kiểm tra nếu response không ok
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        // Chuyển đổi phản hồi sang JSON
+        const result = await response.json();
 
-    const columns = [
-        {
-            name: 'Title',
-            selector: row => row.title,
-        },
-        {
-            name: 'Year',
-            selector: row => row.year,
-        },
-    ];
-    
-    const data = [
-          {
-            id: 1,
-            title: 'Beetlejuice',
-            year: '1988',
-        },
-        {
-            id: 2,
-            title: 'Ghostbusters',
-            year: '1984',
-        },
-    ]
-    
+        // Cập nhật dữ liệu vào state
+        setData(result);
+      } catch (error) {
+        console.log(error);
+      }
+
+      fetchData();
+    };
+    // Gọi hàm fetchData
+    fetchData();
+  }, []);
+
+  const customStyles = {
+    table: {
+      style: {
+        width: '100%', // Giữ bảng vừa với container
+      },
+    },
+  };
+  const columns = [
+    {
+      name: "Customer Name",
+      cell: (row) => {
+        <div className="flex gap-1 items-center">
+          <img src={row.avatar} alt="Avatar" className="size-6 rounded-full" />
+          <span>{row.name}</span>
+        </div>;
+      },
+      sortable: true,
+    },
+    {
+      name: "Company",
+      selector: (row) => row.company,
+      sortable: true,
+    },
+    {
+      name: "Order Value",
+      selector: (row) => row.orderValue,
+      sortable: true,
+      right: true,
+    },
+  ];
   return (
     <div>
-      <DataTable
-			columns={columns}
-			data={data}
-			expandableRows
-			expandableRowsComponent={ExpandedComponent}
-		/>
+      <DataTable columns={columns} data={data} customStyles={customStyles}/>
     </div>
   );
 };
