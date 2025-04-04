e5b107041e395ad4027c556ec26a1f5bf6eed205
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2025-04-04 10:36:50 +0700

    4. Done DataTable API

--- src/components/DataTable/DataTableNe.jsx
+++ src/components/DataTable/DataTableNe.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import DataTable from "react-data-table-component";
 
 const DataTableNe = () => {
   const [data, setData] = useState([]);
@@ -30,39 +30,126 @@ const DataTableNe = () => {
     fetchData();
   }, []);
 
+  const statuses = ["New", "In-progress", "Completed"];
+
   const customStyles = {
     table: {
       style: {
-        width: '100%', // Giữ bảng vừa với container
+        width: "95%", // Giữ bảng vừa với container
+        margin: "auto",
+        color: "black",
+        borderRadius: "10px",
+        border: "0.5px solid gray",
+        overflow: "hidden", // Ngăn các phần tử con vượt ra ngoài
+      },
+    },
+    headRow: {
+      style: {
+        backgroundColor: "#f8f9fa",
+        fontSize: "bold",
+      },
+    },
+    pagination: {
+      style: {
+        width: "95%",
+        margin: "auto",
+        display: "flex",
+        padding: "10px",
+        border: "none",
+      },
+    },
+    paginationButton: {
+      style: {
+        color: "black", // Màu chữ
+        padding: "5px 10px",
+        cursor: "pointer",
       },
     },
   };
   const columns = [
     {
-      name: "Customer Name",
+      name: "CUSTOMER NAME",
       cell: (row) => {
-        <div className="flex gap-1 items-center">
-          <img src={row.avatar} alt="Avatar" className="size-6 rounded-full" />
-          <span>{row.name}</span>
-        </div>;
+        return (
+          <div className="flex gap-1 items-center">
+            <img
+              src={row.avatar}
+              alt="Avatar"
+              className="size-6 rounded-full"
+            />
+            <span>{row.name}</span>
+          </div>
+        );
       },
       sortable: true,
     },
     {
-      name: "Company",
+      name: "COMPANY",
       selector: (row) => row.company,
       sortable: true,
     },
     {
-      name: "Order Value",
-      selector: (row) => row.orderValue,
+      name: "ORDER VALUE",
+      selector: (row) => `$${row.orderValue}`,
       sortable: true,
-      right: true,
+    },
+    {
+      name: "ORDER DATE",
+      selector: (row) => new Date(row.orderDate).toLocaleDateString("vi-VN"), // "dd/mm/yyyy",
+      sortable: true,
+    },
+    {
+      name: "STATUS",
+      selector: (row) => statuses[row.status % 3],
+      cell: (row) => {
+        const status = statuses[row.status % 3];
+        return (
+          <span
+            className={`
+          px-3 py-1 rounded-full text-sm font-medium
+          ${status === "New" ? "bg-blue-100 text-blue-600" : ""}
+          ${status === "In-progress" ? "bg-yellow-100 text-yellow-700" : ""}
+          ${status === "Completed" ? "bg-green-100 text-green-600" : ""}
+        `}
+          >
+            {status}
+          </span>
+        );
+      },
+    },
+    {
+      cell: () => (
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-5 cursor-pointer"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+            />
+          </svg>
+        </span>
+      ),
+      ignoreRowClick: true,
+      allowOverflow: true,
+      button: true,
     },
   ];
   return (
-    <div>
-      <DataTable columns={columns} data={data} customStyles={customStyles}/>
+    <div className="mt-5">
+      <DataTable
+        columns={columns}
+        data={data}
+        customStyles={customStyles}
+        pagination
+        selectableRows
+      />
     </div>
   );
 };
--- src/components/RootLayout/OverviewItem.jsx
+++ src/components/RootLayout/OverviewItem.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import React from "react";
 
-const OverviewItem = ({ title, number, percent, icon }) => {
-    // Giải mã chuỗi SVG (thay thế các ký tự escape)
-    const decodedIcon = decodeURIComponent(icon);
+const OverviewItem = ({ title, number, percent, icon, colorIndex }) => {
+  // Giải mã chuỗi SVG (thay thế các ký tự escape)
+  const decodedIcon = decodeURIComponent(icon);
 
-    return (
-        <div className='bg-white py-2 px-2 w-[300px] rounded-lg relative'>
-            <h3 className='font-bold text-black'>{title}</h3>
-            <h2 className='font-bold text-5xl mt-2  text-black'>{number}</h2>
-            <h4 className='text-[12px] mt-4 flex items-center text-black font-semibold gap-1'>
-                <span className='text-[#387b59] flex items-center'>▲
-                {percent}</span> period of change</h4>
+  return (
+    <div
+      className={`${
+        colorIndex !== 2 ? "bg-[#fef0f5]" : "bg-[#f1f8fd]"
+      } py-3 px-5 w-[340px] rounded-lg relative `}
+    >
+      <h3 className="font-bold text-black">{title}</h3>
+      <h2 className="font-bold text-4xl mt-2 text-black">
+        {colorIndex !== 2 ? `$${number}` : number}
+      </h2>
+      <h4 className="text-[12px] mt-4 flex items-center text-black font-semibold gap-1">
+        <span className="text-[#387b59] flex items-center">▲{percent}</span>{" "}
+        period of change
+      </h4>
 
-            {/* Chèn SVG từ chuỗi đã giải mã */}
-            <span className="absolute top-2 right-2 text-xl text-[#e84f85] border border-[#e84f85] p-1 rounded-md"
-                dangerouslySetInnerHTML={{ __html: decodedIcon }} />
-
-        </div>
-    )
-}
-export default OverviewItem
+      {/* Chèn SVG từ chuỗi đã giải mã */}
+      <span
+        className={`${
+          colorIndex !== 2
+            ? "text-[#e84f85] border-[#e84f85]"
+            : "text-[#2b80ff] border-[#2b80ff]"
+        } absolute top-2 right-2 text-xl  border  p-1 rounded-md`}
+        dangerouslySetInnerHTML={{ __html: decodedIcon }}
+      />
+    </div>
+  );
+};
+export default OverviewItem;
