a7a4ef363e49807f6a34f3dc32655ca88d972a23
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2025-04-05 18:54:46 +0700

    Done 6. Single Page Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
 import RootPage from "./components/RootLayout/RootPage.jsx";
-import Teams from "./components/Teams/Teams.jsx";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<RootPage />} />
-      <Route path="/teams" element={<Teams />} />
-    </Routes>
-  );
+  return <RootPage />;
 }
 
 export default App;
---
+++ src/assets/content/DashBoard.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import DataTableNe from "../../components/DataTable/DataTableNe";
+
+const DashBoard = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-[#e84f85]"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z"
+              clipRule="evenodd"
+            />
+            <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+          </svg>
+          <div className="font-bold text-2xl">Detailed report</div>
+        </div>
+        <div className="flex pr-4 gap-4">
+          <button className="text-[#f44b87] border-[#f44b87] border-[1.5px] flex items-center gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4 font-bold"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+              />
+            </svg>
+            Import
+          </button>
+          <button className="text-[#f44b87] border-[#f44b87] border-[1.5px] flex items-center gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-4"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+            Export
+          </button>
+        </div>
+      </div>
+      <DataTableNe />
+    </div>
+  );
+};
+
+export default DashBoard;
