1a7616e603c428830d902dc24b9e97e2892649d0
bloghaynhat <22683091.duc@student.iuh.edu.vn>
2025-04-05 18:54:46 +0700

    Done 6. Single Page Application

--- src/components/RootLayout/RootPage.jsx
+++ src/components/RootLayout/RootPage.jsx
@@ -1,8 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./RootPage.css";
 import OverviewItem from "./OverviewItem";
-import DataTableNe from "../DataTable/DataTableNe";
 import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import DashBoard from "../../assets/content/DashBoard";
+import Teams from "../../assets/content/Teams";
+import Project from "../../assets/content/Project";
+import Analytics from "../../assets/content/Analytics";
+import Messages from "../../assets/content/Message";
+import Integration from "../../assets/content/Integration";
 
 const RootPage = () => {
   const [dataOverview, setdataOverview] = useState([]);
@@ -44,15 +50,23 @@ const RootPage = () => {
           <div>
             <ul className="flex flex-col gap-y-5 mt-6 [&>li]:p-2 [&>li]:rounded-lg font-bold ">
               <li>
-                <Link to="">Dashboard</Link>{" "}
+                <Link to="/">Dashboard</Link>
               </li>
-              <li>Project</li>
+              <li>
+                <Link to="/project">Project</Link>
+              </li>
               <li>
                 <Link to="/teams">Teams</Link>
               </li>
-              <li>Analytics</li>
-              <li>Messages</li>
-              <li>Integration</li>
+              <li>
+                <Link to="/analytics">Analytics</Link>
+              </li>
+              <li>
+                <Link to="/messages">Messages</Link>
+              </li>
+              <li>
+                <Link to="/integration">Integration</Link>
+              </li>
             </ul>
           </div>
         </div>
@@ -149,64 +163,17 @@ const RootPage = () => {
             })}
           </div>
         </div>
-
+        {/* Phần này để load content */}
         {/* DataTable ne */}
         <div className="mt-10">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="size-6 text-[#e84f85]"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z"
-                  clipRule="evenodd"
-                />
-                <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
-              </svg>
-              <div className="font-bold text-2xl">Detailed report</div>
-            </div>
-            <div className="flex pr-4 gap-4">
-              <button className="text-[#f44b87] border-[#f44b87] border-[1.5px] flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-4 font-bold"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
-                  />
-                </svg>
-                Import
-              </button>
-              <button className="text-[#f44b87] border-[#f44b87] border-[1.5px] flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="size-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-                  />
-                </svg>
-                Export
-              </button>
-            </div>
-          </div>
-          <DataTableNe />
+          <Routes>
+            <Route path="/" element={<DashBoard />}></Route>
+            <Route path="/project" element={<Project />}></Route>
+            <Route path="/teams" element={<Teams />}></Route>
+            <Route path="/analytics" element={<Analytics />}></Route>
+            <Route path="/messages" element={<Messages />}></Route>
+            <Route path="/integration" element={<Integration />}></Route>
+          </Routes>
         </div>
       </div>
     </div>
